da8477bfcf874d09bc9be6f9f43544fe950a05fa
Youngsik Yang <vacu9708@gmail.com>
2023-03-08 02:29:21 +0900

    HTTP delete

---
+++ side projects/MSA beer service with spring/mssc-brewery/src/test/java/guru/springframework/msscbrewery/web/controller/Controller_test.java
@@ -0,0 +1,14 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Controller_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
