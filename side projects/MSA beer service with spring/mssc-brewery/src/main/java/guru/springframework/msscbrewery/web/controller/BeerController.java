456cbe9fddb61244d2ca88e5ea0faa11cdbf2215
Youngsik Yang <vacu9708@gmail.com>
2023-03-10 07:45:53 +0900

    client

--- side projects/MSA beer service with spring/mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ side projects/MSA beer service with spring/mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,6 +2,8 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,13 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
-
     private final BeerService beerService;
-
     public BeerController(BeerService beerService) {
         this.beerService = beerService;
     }
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
-
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
@@ -53,5 +52,4 @@ public class BeerController {
     public void deleteBeer(@PathVariable("beerId") UUID beerId){
         beerService.deleteById(beerId);
     }
-
 }
