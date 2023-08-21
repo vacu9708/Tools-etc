b0179040b2b2c78bc2ad4c5b00b9702bc31cd781
Youngsik Yang <vacu9708@gmail.com>
2023-08-22 07:58:21 +0900

    Delete side projects directory

--- side projects/MSA beer service with spring/readme.md
+++
@@ -1,2 +0,0 @@
-# [Spring Boot Microservices with Spring Cloud Beginner to Guru](https://www.udemy.com/course/spring-boot-microservices-with-spring-cloud-beginner-to-guru/)
-[mssc-beer-service](https://github.com/springframeworkguru/mssc-beer-service)
--- side projects/Sign-in, Sign-up/first_database.sql
+++
@@ -1,8 +0,0 @@
-use first_database;
-
-create table if not exists users(
-	id varchar(100) primary key comment 'user login ID',
-    name varchar(100) not null comment 'user name',
-    age tinyint unsigned not null comment 'user age, maximum 65,000 years old',
-    password varchar(300) not null comment 'login password(encrypted)'
-);
